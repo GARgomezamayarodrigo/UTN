724e8976ef0cc25453745ac071a33c7d7a0b6d74
rodrigo <gomezamayarodrigo@gmail.com>
2023-12-10 22:53:24 -0300

    agregue archivos

--- transportes/front/src/components/layout/Nav.js
+++ transportes/front/src/components/layout/Nav.js
@@ -8,11 +8,11 @@ const Nav = (props) => {
                 <ul>
                     <li><NavLink to="/" className={({ isActive }) => isActive ? "activo" :
                         undefined}>Home</NavLink></li>
-                    <li><NavLink to="/" className={({ isActive }) => isActive ? "activo" :
+                    <li><NavLink to="/nosotros" className={({ isActive }) => isActive ? "activo" :
                         undefined}>Nosotros</NavLink></li>
-                    <li><NavLink to="/" className={({ isActive }) => isActive ? "activo" :
+                    <li><NavLink to="/novedades" className={({ isActive }) => isActive ? "activo" :
                         undefined}>Novedades</NavLink></li>
-                    <li><NavLink to="/" className={({ isActive }) => isActive ? "activo" :
+                    <li><NavLink to="/contacto" className={({ isActive }) => isActive ? "activo" :
                         undefined}>Contacto</NavLink></li>
                 </ul>
             </div>
